994b2d4a6efa2b154e9ad7c83c1e55cb907036a7
mpolyakov84 <mpolyakov7@gmail.com>
2025-06-26 15:47:44 +0200

    Main part of code is ready

--- numberOfOccurrences/main.py
+++ numberOfOccurrences/main.py
@@ -1,1 +1,36 @@
 # App counts how many times a number appears in a list
+norm_list = []
+while True:
+    next_step = False
+    input_numbers = input("Enter a list of numbers separated by a commas: ")
+    input_numbers = input_numbers.split(",")
+    norm_list_tmp = [item.strip() for item in input_numbers]
+    norm_list = []
+    for num in norm_list_tmp:
+        try:
+            num = int(num)
+            norm_list.append(num)
+        except ValueError:
+            print("Error: all inputs have to be number. Please try again.")
+            next_step = True
+            break
+    if not next_step:
+        break
+
+while True:
+    chosen_number = input('Enter the number what you want to count: ')
+    if chosen_number.isdigit():
+        chosen_number = int(chosen_number)
+        break
+    else:
+        print("Error: entered value has to be number. Please try again.")
+
+counter = 0
+for num in norm_list:
+    if num == chosen_number:
+        counter += 1
+
+# print(norm_list, chosen_number, counter)
+print(f'The number {chosen_number} appears {counter} times in the list. ')
+
+# TODO: Add tests
