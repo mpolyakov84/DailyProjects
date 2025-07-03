cd066e7ddc08a7c586b1f50087f6bba2a205c424
mpolyakov84 <mpolyakov7@gmail.com>
2025-07-03 21:17:31 +0200

    Template for unittests has been created with TODOs

---
+++ emailSender/test_mail_sender.py
@@ -0,0 +1,13 @@
+import mail_sender
+import unittest
+
+
+# TODO: check if password not None
+# TODO: check get_recipient_list - negative/positive
+# TODO: check if email correct
+# TODO: check send_email - negative/positive
+
+
+class TestMailSender(unittest.TestCase):
+
+    pass
